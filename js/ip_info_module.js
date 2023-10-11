ec72ff9f4ab77b97f4ec52c88eabd6bdbdc99791
Anatolii-Ovcharuk <ovcharuk.tolik@gmail.com>
2023-10-11 12:55:41 +0300

    Add show information about IP Adress

--- js/ip_info_module.js
+++ js/ip_info_module.js
@@ -4,7 +4,7 @@
 
 
 
-/* VERSION 1 */
+/* -------------- VERSION 1 -------------- */
 
 // fetch('https://ipapi.co/json/')
 //     .then(info => info.json())
@@ -14,30 +14,104 @@
 //     .catch(error => console.log("ERROR", error));
 
 
-/* VERSION 2 */
 
-let dataIP = "Not detected.";
+
+/* -------------- VERSION 2 -------------- */
+
+// let dataIP = "Not detected.";
+// let colorIP = "#f9f522";
+// showIP();
+
+// async function initializateIP () {
+//     await fetch('https://ipapi.co/json/')
+//     .then(info => info.json())
+//     .then(info => {dataIP = info.ip; colorIP = "#26f922";})
+//     .catch(error => console.log("ERROR", error));
+
+//     if (dataIP === "Not detected." || dataIP === undefined || dataIP === false) {
+//         colorIP = "#f92222";
+//         console.log("WARNING! IP Adress not detected.");
+//     }; 
+
+//     showIP();
+// }
+
+// function showIP () {
+// /* SELECT ONLY ONE STRING */
+// document.querySelector('.ip').innerHTML = `You're current IP Adress: <a href="https://en.ipshu.com/picture/${dataIP}.png" style="color:${colorIP};" target="_blank">${dataIP}</a>`;
+// // document.querySelector('#ip').innerHTML = `You're current IP Adress: <a href="https://en.ipshu.com/picture/${dataIP}.png" style="color:${colorIP};" target="_blank">${dataIP}</a>`;
+// }
+
+// initializateIP();
+
+
+
+
+/* -------------- VERSION 3 -------------- */
+
+let dataIP = {
+    ip: "Searching...",
+    asn: "",
+    city: "Unknow city",
+    continent_code: "Unknow continent",
+    country: "",
+    country_area: 0,
+    country_calling_code: "",
+    country_capital: "",
+    country_code: "",
+    country_code_iso3: "",
+    country_name: "Unknow country",
+    country_population: 0,
+    country_tld: "",
+    currency: "",
+    currency_name: "",
+    in_eu: false,
+    languages: "",
+    latitude: "",
+    longitude: "",
+    network: "",
+    org: "",
+    postal: "",
+    region: "Unknow region",
+    region_code: "",
+    timezone: "",
+    utc_offset: "",
+    version: "",
+};
 let colorIP = "#f9f522";
 showIP();
 
+
 async function initializateIP () {
-    await fetch('https://ipapi.co/json/')
-    .then(info => info.json())
-    .then(info => {dataIP = info.ip; colorIP = "#26f922";})
-    .catch(error => console.log("ERROR", error));
+    const response = await fetch('https://ipapi.co/json/'); // Also http://freegeoip.net/json/
+    try {
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        } else {
+            dataIP = await response.json();
+            // console.log(dataIP);
+        }
+    } catch (error) {
+        console.log(error);
+    };
 
-    if (dataIP === "Not detected." || dataIP === undefined || dataIP === false) {
-        colorIP = "#f92222";
-        console.log("WARNING! IP Adress not detected.");
-    }; 
-
+    colorIP = "#26f922";
     showIP();
 }
 
+setTimeout( () => {
+    if (dataIP.ip === "Searching...") {
+        dataIP.ip = "Not detected."
+        colorIP = "#f92222";
+        console.log("WARNING! IP Adress not detected. Problem with connection. Waiting...");
+        showIP();
+    }
+}, 10000);
+
 function showIP () {
 /* SELECT ONLY ONE STRING */
-document.querySelector('.ip').innerHTML = `You're current IP Adress: <a href="https://en.ipshu.com/picture/${dataIP}.png" style="color:${colorIP};" target="_blank">${dataIP}</a>`;
-// document.querySelector('#ip').innerHTML = `You're current IP Adress: <a href="https://en.ipshu.com/picture/${dataIP}.png" style="color:${colorIP};" target="_blank">${dataIP}</a>`;
+document.querySelector('.ip').innerHTML = `You're place: ${dataIP.city}, ${dataIP.region}, ${dataIP.country_name}, ${dataIP.continent_code}. You're current IP Adress: <a href="https://en.ipshu.com/picture/${dataIP.ip}.png" style="color:${colorIP};" target="_blank">${dataIP.ip}</a> ${dataIP.version} ${dataIP.org}`;
+// document.querySelector('#ip').innerHTML = `You're place: ${dataIP.city}, ${dataIP.region}, ${dataIP.country_name}, ${dataIP.continent_code}. You're current IP Adress: <a href="https://en.ipshu.com/picture/${dataIP.ip}.png" style="color:${colorIP};" target="_blank">${dataIP.ip}</a> ${dataIP.version} ${dataIP.org}`;
 }
 
 initializateIP();
